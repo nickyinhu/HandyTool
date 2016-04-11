fcb16c7ea12d328b626e6e34298350d0abf0f748
jwang684 <jwang684@gatech.edu>
2016-04-11 10:48:13 -0400

    how to add accessory

--- Handy/addnewtool.php
+++ Handy/addnewtool.php
@@ -16,6 +16,27 @@
          if (!$login_user) {
             die("You are not login yet!");
          }
+
+         if ( ! empty( $_POST ) and ! empty($_POST['tooltype'])) {
+              // Insert our data
+            $sql = "INSERT INTO tools( tool_id, abbr_description, full_description, purchase_price, rental_price, deposit, tool_type ) 
+                VALUES ( '{$mysqli->real_escape_string($_POST['toolid'])}', '{$mysqli->real_escape_string($_POST['abbr'])}',
+                '{$mysqli->real_escape_string($_POST['full'])}','{$mysqli->real_escape_string($_POST['purchaseprice'])}', 
+                '{$mysqli->real_escape_string($_POST['rentalprice'])}',
+                '{$mysqli->real_escape_string($_POST['deposit'])}', '{$mysqli->real_escape_string($_POST['tool_type'])}')";
+            
+            $insert = $mysqli->query($sql);
+  
+            // Print response from MySQL
+            if ( $insert ) {
+               echo "Success! Row ID: {$mysqli->insert_id}";
+            } else {
+               die("Error: {$mysqli->errno} : {$mysqli->error}");
+            }
+  
+            // Close our connection
+            $mysqli->close();
+            }
       ?>
 
       <form>
@@ -25,10 +46,10 @@
       Deposit Amount ($): <input type="text" name="deposit"><br>
       Full Description: <input type="text" name="full"><br>
       Tool Type: 
-         <select>
-            <option name="construction">Construction</option>
-            <option name="hand">Hand</option>
-             <option name="power">Power</option>
+         <select name="tooltype">
+            <option value="construction">Construction</option>
+            <option value="hand">Hand</option>
+             <option value="power">Power</option>
          </select>
       <div>
       If new item is a Power Tool, then include accessorites: <button type="submit" value="Add">Add Accessories</button>
