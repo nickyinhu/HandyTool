6bb3cfc0f748fa9fc3be214573edf39e3836e3a2
Sun, Pengfei <psun001@gatech.edu>
2016-04-17 18:47:00 -0700

    Update rental_receipt.php

--- Handy/rental_receipt.php
+++ Handy/rental_receipt.php
@@ -29,13 +29,13 @@
 			$res_num = $_SESSION['res_num'];
 			$clerk_id = $_SESSION['login_user'];
 			//find clerk name according to clerk id
-			$sql_clerk = "select first_name, last_name from clerk where clerk_id = $clerk_id";
+			$sql_clerk = "select first_name, last_name from clerk where clerk_id = '$clerk_id'";
 			$result_clerk = $conn->query($sql_clerk) or die("Error querying the database");
 			$row_clerk = $result_clerk->fetch_assoc();
 			//display clerk name
 			echo "Clerk on Duty: ".$row_clerk['first_name']." ".$row_clerk['last_name']."<br>";
 			//update reservation table, add dropoff clerk id
-			$sql_dropoff_clerk = "UPDATE reservation SET dropoff_clerk_id = '$clerk_id' where resv_number = '$res_num'";
+			$sql_dropoff_clerk = "UPDATE reservation SET dropoff_clerk_id = '$clerk_id' where resv_number = $res_num";
 			$conn->query($sql_dropoff_clerk) or die("Error update database");
 
 			//customer name
@@ -45,7 +45,7 @@
 			$row_customer = $result_customer->fetch_assoc();
 			$customer_email = $row_customer['customer_email'];
 			//find out customer name according to email
-			$sql_cus_name =  "select first_name, last_name from customer where email = $customer_email";
+			$sql_cus_name =  "select first_name, last_name from customer where email = '$customer_email'";
 			$result_cus_name = $conn->query($sql_cus_name);
 			$row_cus_name = $result_cus_name->fetch_assoc();
 			echo "Customer Name: ". $row_cus_name['first_name']." ".$row_cus_name['last_name']." ";
